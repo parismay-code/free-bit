ba8b5ec8586e4f635d2d42e63874c1947bbb3916
Kirill Kuznetsov <kka.fullstack@gmail.com>
2024-01-25 19:48:01 +0300

    resources

--- app/Http/Resources/OrganizationResource.php
+++ app/Http/Resources/OrganizationResource.php
@@ -14,6 +14,11 @@ class OrganizationResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'owner' => new UserResource($this->owner()),
+        ];
     }
 }
